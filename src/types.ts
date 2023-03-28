16bcad27421caf9847cfcedd35a0c783d4b64683
TUYIZERE Samuel <me@samueltuyizere.com>
2023-03-28 17:50:57 +0200

    added further types declarations
    Signed-off-by: TUYIZERE Samuel <me@samueltuyizere.com>

--- src/types.ts
+++ src/types.ts
@@ -11,3 +11,40 @@ export interface intouchWebhook {
   status:string,
   statusdesc:string,
 }
+
+enum RequestDepositStatus {
+  'Successfull'='Successfull'
+}
+export declare interface RequestDepositResponse {
+  status:RequestDepositStatus,
+  message:string,
+  success:boolean,
+  referenceid:string,
+  responsecode:string,
+  transactionid:string,
+  requesttransactionid:string
+}
+
+enum RequestPaymentStatus {
+  'Pending'='Pending'
+}
+
+export declare interface RequestPaymentResponse {
+  status:RequestPaymentStatus,
+  message:string,
+  success:boolean,
+  responsecode:string,
+  transactionid:string,
+  requesttransactionid:string
+}
+
+export declare interface GetBalanceSuccessResponse{
+  balance:string,
+  success: boolean
+}
+
+export declare interface GetBalanceFailureResponse{
+  success:boolean,
+  responsecode:string,
+  message: string
+}
